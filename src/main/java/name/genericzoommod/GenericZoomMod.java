1e0bab57f3e15601ba5b89963ccde39c649bf799
TCL03 <khoanguyenanh513@gmail.com>
2025-06-24 08:56:09 +0700

    initialize files

---
+++ src/main/java/name/genericzoommod/GenericZoomMod.java
@@ -0,0 +1,16 @@
+package name.genericzoommod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GenericZoomMod implements ModInitializer {
+	public static final String MOD_ID = "generic-zoom-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/name/genericzoommod/GenericZoomModDataGenerator.java
@@ -0,0 +1,11 @@
+package name.genericzoommod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GenericZoomModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
